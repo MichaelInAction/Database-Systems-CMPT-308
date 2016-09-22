13e6db0ab895ad944eddb38da83cb9ef9ec8b399
MichaelInAction <michael.read.75@gmail.com>
2016-09-21 20:56:09 -0400

    Uploading Lab 3 - 9/21/16 - Michael Read
    Michael Read uploaded the Lab 3 .sql file to the repository on September
    21, 2016

---
+++ Lab3.sql
@@ -0,0 +1,48 @@
+﻿--Michael Read--
+--Lab 3--
+
+--Question 1--
+select ordnum, totalUSD
+from Orders;
+
+--Question 2--
+select name, city
+from Agents
+where name = 'Smith';
+
+--Question 3--
+select pid, name, priceUSD
+from Products
+where quantity > 201000;
+
+--Question 4--
+select name, city
+from Customers
+where city = 'Duluth';
+
+--Question 5--
+select name
+from Agents
+where not (city iN ('New York', 'Duluth'));
+
+--Question 6--
+select *
+from Products
+where not (city in ('Dallas', 'Duluth'))
+  and priceUSD >= 1;
+
+--Question 7--
+select *
+from Orders
+where mon in ('feb', 'mar');
+
+--Question 8--
+select *
+from Orders
+where mon = 'feb'
+  and totalUSD >= 600;
+
+--Question 9--
+select *
+from Orders
+where cid = 'c005';
